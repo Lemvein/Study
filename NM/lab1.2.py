a77dd60ef6c526eb8cb5efb75f2e314111a22803
Lemvein <64874138+Lemvein@users.noreply.github.com>
2022-06-22 09:40:45 +0300

    Add files via upload

---
+++ NM/lab1.2.py
@@ -0,0 +1,59 @@
+#Входные данные
+A2 = [
+[8, -4, 0, 0, 0],
+[-2, 12, -7, 0, 0],
+[0, 2, -9, 1, 0],
+[0, 0, -8, 17, -4],
+[0, 0, 0, -7, 13]
+]
+B2 = [32, 15, -10, 133, -76]
+
+#Проверка на корректность
+def Chek(a):
+    n = len(a)
+    k = 0
+    for str in range(0, n):
+        if( len(a[str]) != n ):
+            print('Не соответствует размерность')
+            return False
+    for i in range(1, n - 1):
+        if (abs(a[i][i]) >= abs(a[i][i-1]) + abs(a[i][i+1])):
+            if (abs(a[i][i]) > abs(a[i][i-1]) + abs(a[i][i+1])): 
+                k += 1      
+        else:
+            print('Не выполнены условия достаточности')
+            return False
+        if k == 0:
+            print('Не выполнены условия достаточности')
+            return False
+    for stroka in range(0, len(a)):
+        if( a[str][str] == 0 ):
+            print('Нулевые элементы на главной диагонали')
+            return False
+    return True
+ 
+#Метод прогонки
+def Progon(A, B):
+    if (not Chek(A)):
+        print('Ошибка в исходных данных')
+        return 0 
+    n = len(A)
+    x = [0 for k in range(0, n)]   
+    #Прогоночные коэффициенты  
+    a = [0 for k in range(0, n)]
+    b = [0 for k in range(0, n)]
+    a[0] = -A[0][1] / A[0][0] 
+    b[0] = B[0] / A[0][0] 
+    for i in range(1, n - 1):
+        a[i] = -A[i][i+1] / ( A[i][i] + A[i][i-1]*a[i-1] )
+        b[i] = (B[i] - A[i][i-1]*b[i-1] ) / ( A[i][i] + A[i][i-1]*a[i-1] )
+    a[n-1] = 0
+    b[n-1] = (B[n-1] - A[n-1][n-2]*b[n-2]) / (A[n-1][n-1] + A[n-1][n-2]*a[n-2])
+    #Обратный ход
+    x[n-1] = b[n-1] 
+    for i in range(n-1, 0, -1):
+        x[i-1] = a[i-1] * x[i] + b[i-1] 
+    return x 
+
+print("Ответ:")
+print("\n".join("X{0} =\t{1:10.2f}".format(i + 1, j) for i, j in enumerate(Progon(A2, B2))))
