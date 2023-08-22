f8f6a3db32f446f9e592bd4336a0ee8d3ca000b6
Tatyana Danilova <64874138+Lemvein@users.noreply.github.com>
2023-08-22 17:34:49 +0300

    Add files via upload

---
+++ ISRPPS/lab15_WindowsFormsApp/lab15_WindowsFormsApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using lab15_ClassLibrary;
+
+namespace lab15_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        Reestr reestr = new Reestr();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = reestr.F + " " + reestr.I + " " + reestr.year;
+        }
+    }
+}
